3bf9398afbddaa78c16ff8cf9b464a58051cb6c4
Daniel O'Connor <clockwerx@php.net>
2008-12-16 12:07:34 +0000

    Bug #15318 - Improve unit tests (less redundant code!)
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_Feed_Parser/trunk@271337 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/convertedtests/feedburner.php
+++ tests/convertedtests/feedburner.php
@@ -6,12 +6,9 @@ class feedburner_TestCase extends XML_Feed_Parser_Converted_TestCase {
 
     function test_feedburner_browserfriendly_1 () { 
         $content = file_get_contents($this->fp_test_dir . DIRECTORY_SEPARATOR . 'wellformed/feedburner/feedburner_browserfriendly.xml');
-        try {
-            $feed = new XML_Feed_Parser($content);
-        } catch (XML_Feed_Parser_Exception $e) {
-            $this->assertTrue(false);
-            return;
-        }
+
+        $feed = new XML_Feed_Parser($content);
+
         $this->assertEquals('This is an XML content feed. It is intended to be viewed in a newsreader or syndicated to another site.', $feed->info);
     }
 }
