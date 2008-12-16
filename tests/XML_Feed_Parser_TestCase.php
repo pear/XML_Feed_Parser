ffa364f69bf548b1f2fa72273bec7265ae9c7e5c
Daniel O'Connor <clockwerx@php.net>
2008-12-16 13:24:23 +0000

    Bug #15318 - actually let unit tests run!
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_Feed_Parser/trunk@271343 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/XML_Feed_Parser_TestCase.php
+++ tests/XML_Feed_Parser_TestCase.php
@@ -17,8 +17,8 @@ abstract class XML_Feed_Parser_Converted_TestCase extends XML_Feed_Parser_TestCa
     function setup() {
         $this->fp_test_dir = XML_Feed_Parser_TestCase::getSampleDir() . 
             DIRECTORY_SEPARATOR . 'feedparsertests';
-        if (! is_dir($fp_test_dir)) {
-            throw new Exception('Feed parser tests must be unpacked into the folder ' . 
+        if (! is_dir($this->fp_test_dir)) {
+            $this->markTestSkipped('Feed parser tests (http://code.google.com/p/feedparser/downloads/list) must be unpacked into the folder ' . 
                 $this->fp_test_dir);
         }
     }
